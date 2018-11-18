158d77eb15cea1e6f8933460915ede7c61f6d03e
Prashant Patel <prashant.n.patel@ibm.com>
2018-11-18 15:28:05 -0800

    Added the python solution for question771

---
+++ questions/771/python/Solution.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def numJewelsInStones(self, J, S):
+        """
+        :type J: str
+        :type S: str
+        :rtype: int
+        """
+        if len(J)==0 or len(S)==0:
+            return 0;
+        answer=0
+        J_set = set(J)
+        for char in S:
+            if char in J_set:
+                answer+=1
+        
+        return answer
