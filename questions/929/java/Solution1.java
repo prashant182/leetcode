5e018cab4bcd280ccd33e60a6bdc6768f3096e01
Prashant Patel <prashant.n.patel@ibm.com>
2018-11-18 15:56:49 -0800

    Added the Java solution for 929 and Readme explianing the question

---
+++ questions/929/java/Solution1.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int numUniqueEmails(String[] emails) {
+        if (emails.length >100 || emails.length==0){
+            return 0;
+        }
+        HashSet<String> emailSet = new HashSet<>();
+        String localName ="";
+        String domainName="";
+        for (String s: emails){
+            if (s.length()==0){
+                return 0;
+            }
+            String[] splitEmail = s.split("@",2);
+            localName = splitEmail[0];
+            domainName= splitEmail[1];
+            localName = processLocalName(localName);
+            String newDomain = localName+domainName;
+            emailSet.add(newDomain);
+        }
+        
+        return emailSet.size();
+    }
+    
+    public String processLocalName(String input){
+        String ans="";
+        //Check if there is a plus in the string
+        if (input.contains("+")){
+            //Remove everything after +
+            int plusIndex = input.indexOf("+");
+            ans = input.substring(0,plusIndex);
+        }
+        ans = ans.replace(".","");
+        return ans;
+    }
+}
